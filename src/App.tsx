31e77951366d22185bb16adb9c6129c4f742d903
Ahmedka10 <Ahmedkhadar700@gmail.com>
2023-08-08 11:43:07 +0300

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem pl="2" bg="orange.300" area={"nav"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem pl="2" bg="pink.300">
+          Aside
+        </GridItem>
+        <GridItem pl="2" bg="green.300">
+          Main
+        </GridItem>
+      </Show>
+    </Grid>
+  );
 }
 
 export default App;
