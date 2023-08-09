911b6dab68d0c7cea75b009a76950495c4d99775
Ahmedka10 <Ahmedkhadar700@gmail.com>
2023-08-09 15:45:35 +0300

    clean up the game cards

--- src/components/CardGame.tsx
+++ src/components/CardGame.tsx
@@ -1,7 +1,7 @@
 import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
-import CriticScore from "./criticScore";
+import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/ImageUrl";
 
 interface Props {
@@ -13,13 +13,13 @@ const CardGame = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
