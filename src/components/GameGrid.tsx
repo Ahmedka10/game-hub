d2423f26de388ae5c0186d975192ec1219b5d65d
Ahmedka10 <Ahmedkhadar700@gmail.com>
2023-08-10 09:54:03 +0300

    refrecting gamegride

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,27 +14,26 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 5, 6];
 
+  if (error) return error && <p>{error}</p>;
+
   return (
-    <>
-      {error && <p>{error}</p>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 4, xl: 3 }}
-        padding="10px"
-        spacing={6}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <CardGame game={game} />
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 4, xl: 3 }}
+      padding="10px"
+      spacing={6}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <CardGame game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
